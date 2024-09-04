9f5db7ded806b993b38cf0344eb98b216e56fc98
tenxiaodao <1649844077@qq.com>
2024-09-04 09:56:08 +0800

    Site updated: 2024-09-04 09:56:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/03/GitHub搭建个人网站/","2024/09/03/hexo疑难解答/","2024/09/02/关于网站/","2024/09/03/Hexo主题配置教程/","2024/09/02/日本无条件投降79周年/"];function toRandomPost(){
+var posts=["2024/09/03/GitHub搭建个人网站/","2024/09/04/Hexo如何写文章/","2024/09/03/Hexo主题配置教程/","2024/09/03/hexo疑难解答/","2024/09/02/日本无条件投降79周年/","2024/09/02/关于网站/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
